2692be77d67fd612537705f4ff4e3325cf2cee0d
Chirkova Kseniya <ksu@Chirkovas-MacBook-Pro.local>
2020-04-07 01:20:11 +0300

    comment test,run mix format

--- test/cookpod_web/controllers/page_controller_test.exs
+++ test/cookpod_web/controllers/page_controller_test.exs
@@ -1,8 +1,8 @@
 defmodule CookpodWeb.PageControllerTest do
   use CookpodWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    # conn = get(conn, "/")
-    # assert html_response(conn, 200) =~ "Добро пожаловать в Phoenix!"
-  end
+  # test "GET /", %{conn: conn} do
+  # conn = get(conn, "/")
+  # assert html_response(conn, 200) =~ "Добро пожаловать в Phoenix!"
+  # end
 end
