74cb73dc2416b17674c1fcece3ef82fd38e37809
Chirkova Kseniya <ksu@Chirkovas-MacBook-Pro.local>
2020-04-07 16:50:20 +0300

    Add test fo /terms

--- test/cookpod_web/controllers/page_controller_test.exs
+++ test/cookpod_web/controllers/page_controller_test.exs
@@ -5,4 +5,9 @@ defmodule CookpodWeb.PageControllerTest do
     conn = get(conn, "/")
     assert html_response(conn, 200) =~ "Добро пожаловать в Phoenix!"
   end
+
+  test "GET /terms", %{conn: conn} do
+    conn = get(conn, "/terms")
+    assert html_response(conn, 200)
+  end
 end
