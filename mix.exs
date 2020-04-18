2039fb3ad17b6bc56bbedc3e6b67162f3d2ada1f
Chirkova Kseniya <ck@strahovkaru.ru>
2020-04-18 19:16:16 +0300

    ry add codecov

--- mix.exs
+++ mix.exs
@@ -13,10 +13,7 @@ defmodule Cookpod.MixProject do
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
-        coveralls: :test,
-        "coveralls.detail": :test,
-        "coveralls.post": :test,
-        "coveralls.html": :test
+        "coveralls.json": :test
       ],
       elixirc_options: [warnings_as_errors: true]
     ]
